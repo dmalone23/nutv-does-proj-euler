59c0b43a7f94d4cdb04fa4fe6b87662e32be756b
dmalone23 <malone.da@husky.neu.edu>
2022-05-20 16:12:10 -0400

    complete problem 8

--- malone/08/sol.py
+++ malone/08/sol.py
@@ -1,7 +1,12 @@
 import functools
 
-number = 123123123123998999123123123123
-desired_sequence_length = 4
+number = []
+with open('number.txt','r') as file:
+    for line in file:
+        number.append(line.strip())
+number = int(''.join(number))
+
+desired_sequence_length = 13
 
 def int_to_int_arr(x):
     return list(map(int,[x for x in str(x)]))
@@ -10,21 +15,29 @@ def largest_series_product(x, desired_sequence_length):
     x_list = int_to_int_arr(x)
     current_sequence = [x_list[0]]
     current_sequence_length = len(current_sequence)
-    current_product = functools.reduce(lambda x, y: x * y,current_sequence,1)
+    current_sequence_product = functools.reduce(lambda x, y: x * y,current_sequence,1) # fold function in python
+    current_optimal_product = 0
     position = 0
     while position < len(x_list):
-        # TODO: variables for best current product, etc (like p-values for weighted interval scheduling)
-        # TODO: if x_list[position] == 0, then clear the array and continue to next iteration
         if x_list[position] == 0:
-            pass
-        if current_sequence_length == desired_sequence_length:
-            # TODO: check if x_list[position] * (product of all elements in current sequence minus the first one) >= current optimal product;
-            # TODO: if it is, then remove the element at the front of the list and push x_list[position] to the back of the list
-            # TODO: example: if we have [1, 2, 3, 4, 5] and we want to add 6, then we get [2, 3, 4, 5, 6] (remove 1 from front, and push 6 to back) (like q queue)
-            pass
-        else:
-            # TODO: check if x_list[position] * current_product >= current optimal product; if it is, then add x_list[position] to the current sequence
-            pass
-        # TODO: recalculate everything
+            current_sequence.clear()
+            position += 1
+            continue
 
-largest_series_product(number,4)
+        if current_sequence_length == desired_sequence_length:
+            if x_list[position] * functools.reduce(lambda x, y: x * y,current_sequence[1:],1) >= current_optimal_product:
+                current_optimal_product = x_list[position] * functools.reduce(lambda x, y: x * y,current_sequence[1:],1)
+            current_sequence = current_sequence[1:]
+            current_sequence.append(x_list[position])
+        else:
+            if x_list[position] * current_sequence_product >= current_optimal_product:
+                current_optimal_product = x_list[position] * current_sequence_product
+            current_sequence.append(x_list[position])
+
+        position += 1
+        current_sequence_length = len(current_sequence)
+        current_sequence_product = functools.reduce(lambda x, y: x * y,current_sequence,1)
+    
+    return current_optimal_product
+
+print(largest_series_product(number,desired_sequence_length))
