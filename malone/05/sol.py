33d43bc544db07e87b2072dba87951783c55e7ec
dmalone23 <malone.da@husky.neu.edu>
2022-05-20 15:41:41 -0400

    Complete problem 5

--- malone/05/sol.py
+++
@@ -1,8 +0,0 @@
-# TODO: HINT: use math properties, 1*2*3*4*5*6*7*8*9*10?
-def dbf20(x):
-  for i in range(1,21):
-    if x % i != 0:
-      print(f"*** {x} NOT DIVISIBLE BY {i}")
-      break
-    else:
-      print(f"{x} divisible by {i}")
